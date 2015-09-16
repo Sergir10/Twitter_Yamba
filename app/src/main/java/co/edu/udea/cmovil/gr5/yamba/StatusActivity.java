98f0dbd770cbe8fcbc218cc562dce698b05d3599
Sergio Giraldo <sergir10@gmail.com>
2015-09-16 16:19:39 -0500

    "Posteo terminado correctamente"

--- app/src/main/java/co/edu/udea/cmovil/gr5/yamba/StatusActivity.java
+++ app/src/main/java/co/edu/udea/cmovil/gr5/yamba/StatusActivity.java
@@ -1,6 +1,11 @@
+
 package co.edu.udea.cmovil.gr5.yamba;
 
-import android.support.v7.app.AppCompatActivity;
+import com.thenewcircle.yamba.client.YambaClient;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -8,29 +13,82 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends AppCompatActivity implements View.OnClickListener {
+public class StatusActivity extends Activity {
 
-    private static final String TAG = "StatusActivity";
-    private EditText editStatus;
-    private Button buttonTweet;
+    // Instanciar.
+    private static final String TAG = StatusActivity.class.getSimpleName();
+    private Button mButtonTweet;
+    private EditText mTextStatus;
+    private TextView mTextCount;
+    private int mDefaultColor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
-        editStatus = (EditText) findViewById(R.id.editStatus);
-        buttonTweet = (Button) findViewById(R.id.buttonTweet);
+        // Inicializar.
 
-        buttonTweet.setOnClickListener(this);
+        mButtonTweet = (Button) findViewById(R.id.status_button_tweet);
+        mTextStatus = (EditText) findViewById(R.id.status_text);
+        mTextCount = (TextView) findViewById(R.id.status_text_count);
+        mTextCount.setText(Integer.toString(140));
+        mDefaultColor = mTextCount.getTextColors().getDefaultColor();
+
+        mButtonTweet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String status = mTextStatus.getText().toString();
+                PostTask postTask = new PostTask();
+                postTask.execute(status);
+                Log.d(TAG, "onClicked");
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view){
-        String status = editStatus.getText().toString();
-        Log.d(TAG, "onClicked with status: " + status);
+
+    /* CLASE POST TASK */
+    public class PostTask extends AsyncTask<String, Void, String> {
+
+        private ProgressDialog progress;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(StatusActivity.this,"Posting", "Please wait...");
+            progress.setCancelable(true);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                YambaClient cloud = new YambaClient("student", "password");
+                cloud.postStatus(params[0]);
+
+                Log.d(TAG, "Successfully posted to the cloud: " + params[0]);
+                return "Successfully posted";
+
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to post to the cloud", e);
+                e.printStackTrace();
+                return "Failed to post";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progress.dismiss();
+            if (this != null && result != null) {
+                Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+    /* FIN DE CLASE POST TASK*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
